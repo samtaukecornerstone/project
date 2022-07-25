380da2eacfcdd41cbd749922b804338e0f70a249
samtaukecornerstone <stauke@cornerstone.com>
2022-07-25 18:37:19 +0000

    wrote some tests

---
+++ ussie/tests/testthat/test-matches.R
@@ -0,0 +1,13 @@
+
+test_that("uss_make_matches works", {
+    england <-uss_make_matches(engsoccerdata::england,"England")
+    expect_true(tibble::is_tibble(england))
+
+    expect_named(england,
+                 c("country", "tier", "season", "date", "home", "visitor", "goals_home", "goals_visitor"))
+
+    expect_identical(england$country,rep("England",nrow(england)))
+
+    expect_s3_class(england$tier, "factor")
+})
+
